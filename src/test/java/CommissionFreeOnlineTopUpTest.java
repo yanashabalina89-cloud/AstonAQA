9595b2cf928a7c40d5f9ffd1ccd8fd14a67c0aee
Yana <yana.dresvina@mail.ru>
2026-01-20 20:29:13 +0300

    new commit in Lesson 2.9

--- src/test/java/CommissionFreeOnlineTopUpTest.java
+++ src/test/java/CommissionFreeOnlineTopUpTest.java
@@ -3,15 +3,14 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
@@ -44,15 +43,15 @@ public class CommissionFreeOnlineTopUpTest {
 
     @Test
     void testNameSection() {
-        String header = section.findElement(By.tagName("h2")).getText().replaceAll("\\s+", " ")
+        String title = section.findElement(By.tagName("h2")).getText().replaceAll("\\s+", " ")
                 .trim();
-        assertEquals("Онлайн пополнение без комиссии",header);
+        assertEquals("Онлайн пополнение без комиссии", title);
     }
 
     @Test
     void testPaymentSystemLogos() {
         Set<String> expectedLogos = Set.of("Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкарт");
-        List<WebElement> paymentLogos = driver.findElements(By.cssSelector(".pay__partners img"));
+        List<WebElement> paymentLogos = section.findElements(By.cssSelector(".pay__partners img"));
 
         for (String logo : expectedLogos) {
             assertTrue(paymentLogos.stream().anyMatch(img -> logo.equals(img.getAttribute("alt"))),
@@ -62,14 +61,49 @@ public class CommissionFreeOnlineTopUpTest {
 
     @Test
     void testMoreAboutService() {
+        long start = System.currentTimeMillis();
+        WebElement link = section.findElement(By.tagName("a"));
+//        WebElement link = new WebDriverWait(driver, Duration.ofSeconds(5))
+//                .until(ExpectedConditions.elementToBeClickable(By.linkText("Подробнее о сервисе")));//Долго
 
-        WebElement link = driver.findElement(By.linkText("Подробнее о сервисе"));
         link.click();
-        new WebDriverWait(driver, Duration.ofSeconds(5)).until(ExpectedConditions
+        System.out.println("Клик занял: " + (System.currentTimeMillis() - start) + " мс");
+        new WebDriverWait(driver, Duration.ofSeconds(3)).until(ExpectedConditions
                 .urlContains("/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"));
 
-        assertTrue(driver.getCurrentUrl().contains("/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/"),
-                "Переход не произошёл или URL неверный");
+
         driver.navigate().back();
+    }
+
+    @Test
+    void testContinue() {
+        String expectedPhone = "297777777";
+        String expectedSum = "450";
+        WebElement phone = section.findElement(By.id("connection-phone"));
+        phone.click();
+        phone.sendKeys(expectedPhone);
+        WebElement sum = section.findElement(By.id("connection-sum"));
+        sum.click();
+        sum.sendKeys(expectedSum);
+        WebElement email = section.findElement(By.id("connection-email"));
+        email.click();
+        email.sendKeys("yana.dresvina@mail.ru");
+        WebElement continueButton = section.findElement(By.id("pay-connection")).findElement(By.tagName("button"));
+        continueButton.click();
+
+
+        //WebElement iframe = driver.findElement(By.className("payment-widget-app"));
+        WebElement iframe = new WebDriverWait(driver, Duration.ofSeconds(20))
+                .until(ExpectedConditions.presenceOfElementLocated(By.className("payment-widget-iframe")));
+        driver.switchTo().frame(iframe);
+
+        WebElement popup = driver.findElement(By.className("payment-page__container"));
+
+        WebElement sumInPopup = popup.findElement(By.cssSelector("pay-description__cost span"));
+        String actualSum = sumInPopup.getText();
+        BigDecimal actualSumDecimal = new BigDecimal(actualSum);
+        BigDecimal expectedSumDecimal = new BigDecimal(expectedSum);
+        assertTrue(actualSumDecimal.compareTo(expectedSumDecimal) == 0, "Суммы не совпадают");
+
     }
 }
